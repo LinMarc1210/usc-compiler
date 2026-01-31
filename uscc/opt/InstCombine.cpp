3a9e398e7a8e15fd6d8f2edcc1c6c585c8b75add
LinMarc1210 <marc1210899@gmail.com>
2026-01-31 18:28:08 -0500

    feat: Inst Combine case 1 & 2

--- uscc/opt/InstCombine.cpp
+++ uscc/opt/InstCombine.cpp
@@ -23,6 +23,7 @@
 #include <llvm/IR/CFG.h>
 #include <set>
 #include <queue>
+#include <vector>
 
 using namespace llvm;
 
@@ -34,9 +35,129 @@ namespace opt
 bool InstCombine::runOnFunction(Function& F)
 {
 	bool changed = false;
+	std::vector<Instruction*> toErase;
 
     // PA1: Implement Part 1 ~ 5 of instcombine optimization
+	for (BasicBlock& BB : F) {  // Iterate basic block in function
+		for (Instruction& I : BB) {  // Iterate instruction in the basic block
+			BinaryOperator *BI = dyn_cast<BinaryOperator>(&I);
+			if (BI) {   // if not BinaryOpt, return nullptr to BI
+				ConstantInt *lhs = dyn_cast<ConstantInt>(BI->getOperand(0));  // if not ConstantInt, return nullptr to lhs
+				ConstantInt *rhs = dyn_cast<ConstantInt>(BI->getOperand(1));  // if not ConstantInt, return nullptr to rhs
 
+				// Part 1: Local Constant folding
+				if (lhs && rhs) {
+					APInt result = lhs->getValue();
+					bool didCalc = true;
+					switch (BI->getOpcode()) 
+					{
+						case Instruction::Add:
+							result += rhs->getValue();
+							break;
+						case Instruction::Sub:
+							result -= rhs->getValue();
+							break;
+						case Instruction::Mul:
+							result *= rhs->getValue();
+							break;
+						case Instruction::SDiv:
+							if (rhs->getValue() == 0) {
+								didCalc = false;
+							} else {
+								result = result.sdiv(rhs->getValue());
+							}
+							break;
+						case Instruction::SRem:
+							if (rhs->getValue() == 0) {
+								didCalc = false;
+							} else {
+								result = result.srem(rhs->getValue());
+							}
+							break;
+						case Instruction::And:
+							result &= rhs->getValue();
+							break;
+						case Instruction::Or:
+							result |= rhs->getValue();
+							break;
+						default:
+							didCalc = false;
+							break;
+					}
+					
+					if (didCalc) {
+						// Replace the instruction with the constant result
+						BI->replaceAllUsesWith(ConstantInt::get(BI->getType(), result));
+						// Do not erase the instruction when iterating over the basic block
+						toErase.push_back(&I);
+						changed = true;
+					}
+				}
+				// Part 2: Algebraic Simplication Constant rhs
+				else if (rhs) {
+					Value* replacement = nullptr;    // operand lhs or constant
+					Instruction::BinaryOps newOp = Instruction::BinaryOpsEnd;   // shl inst
+					bool didCalc = true;
+					switch (BI->getOpcode()) 
+					{
+						case Instruction::Add:
+						case Instruction::Sub:
+						case Instruction::Or:
+							if (rhs->getValue() == 0) {
+								replacement = BI->getOperand(0);
+							} else {
+								didCalc = false;
+							}
+							break;
+						case Instruction::Mul:
+							if (rhs->getValue() == 0) {
+								replacement = ConstantInt::get(BI->getType(), 0);
+							} else if (rhs->getValue() == 1) {
+								replacement = BI->getOperand(0);
+							} else if (rhs->getValue().isPowerOf2()) {
+								newOp = Instruction::Shl;
+							} else {
+								didCalc = false;
+							}
+							break;
+						case Instruction::And:
+							if (rhs->getValue().isAllOnesValue()) {
+								replacement = BI->getOperand(0);
+							} else {
+								didCalc = false;
+							}
+							break;
+						default:
+							didCalc = false;
+							break;
+					}
+					if (didCalc) {
+						if (replacement) {
+							BI->replaceAllUsesWith(replacement);
+						}
+						else if (newOp != Instruction::BinaryOpsEnd) {
+							APInt shiftAmt(BI->getType()->getIntegerBitWidth(), rhs->getValue().logBase2());
+							// Create a new shift instruction
+							BI->replaceAllUsesWith(BinaryOperator::Create(
+								newOp,
+								BI->getOperand(0),
+								ConstantInt::get(BI->getType(), shiftAmt),
+								"",
+								BI
+							));
+						}
+						toErase.push_back(&I);
+						changed = true;
+					}
+				}
+			}
+		}
+	}
+
+	// Safe deletion after iteration
+	for (Instruction* inst : toErase) {
+		inst->eraseFromParent();
+	}
 	return changed;
 }
 
