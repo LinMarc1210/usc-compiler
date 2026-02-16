a1ea415075f1c11682ad3ed092ee27a28f968f3d
LinMarc1210 <marc1210899@gmail.com>
2026-02-16 16:36:12 -0500

    feat: naive CFG edge profiling

--- uscc/opt/EdgeProfiling.cpp
+++ uscc/opt/EdgeProfiling.cpp
@@ -48,6 +48,46 @@ bool EdgeProfiling::runOnFunction(Function& F)
 
     //   PA2: Implement
 
+    // assign deterministic basic block IDs, enumerate all CFG edges.
+    collectEdgesAndAssignBlockIds(F, data);
+    // create global last_src and edge_counts
+    createEdgeProfilingGlobals(F, data);
+
+    // instrument each basic block with logsrc/logdest calls
+    IntegerType *i32Ty = IntegerType::getInt32Ty(ctx);
+    IntegerType *i64Ty = IntegerType::getInt64Ty(ctx);
+    ConstantInt* numBlocksConst = ConstantInt::get(IntegerType::getInt64Ty(ctx), data.numBlocks);
+    for (auto &bb : F) {
+        unsigned id = data.blockId[&bb];
+        IRBuilder<> builder(&bb);
+        // logdest step 1: insertion point
+        builder.SetInsertPoint(bb.getFirstNonPHI());
+
+        Value *lastSrc = builder.CreateLoad(data.lastSrcGlobal);   // member variable
+        Value *lastSrc64 = builder.CreateZExt(lastSrc, i64Ty);   // zero to extend
+
+        // logdest step 2: index = last src * numBlocks + id
+        Value *idConst64 = ConstantInt::get(i64Ty, id);
+        Value *mul = builder.CreateMul(lastSrc64, numBlocksConst);
+        Value *index = builder.CreateAdd(mul, idConst64);
+
+        // logdest step 3: obtain a pointer to the corresponding counter entry in edge counts by GEP
+        Value *gepIdx[] = {ConstantInt::get(i64Ty, 0), index};
+        Value *counterPtr = builder.CreateGEP(data.edgeMatrix, gepIdx);
+
+        // logdest step 4: create IR to load the counter value, add 1, and store the incremented value back.
+        Value *count = builder.CreateLoad(counterPtr);
+        Value *newCount = builder.CreateAdd(count, ConstantInt::get(i64Ty, 1));
+        builder.CreateStore(newCount, counterPtr);
+
+        // logsrc step 1: 
+        builder.SetInsertPoint(bb.getTerminator());
+        ConstantInt *idConst32 = ConstantInt::get(i32Ty, id);   // not null if type is consistent
+        builder.CreateStore(idConst32, data.lastSrcGlobal);
+    }
+
+    // printing
+    insertEdgeProfilePrinting(F, data, "naive");
     return true;
 }
 
