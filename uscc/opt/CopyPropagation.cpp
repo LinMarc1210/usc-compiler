da41cb39c77b61b58e0f5811b0558e2b110c898d
LinMarc1210 <marc1210899@gmail.com>
2026-03-26 03:14:58 -0400

    feat: Local Copy Propagation for array After SSA

--- uscc/opt/CopyPropagation.cpp
+++ uscc/opt/CopyPropagation.cpp
@@ -30,6 +30,73 @@ namespace opt
 		// invalidates all forwarding entries for that array base.
 		// Clear the map on function calls (may modify memory).
 
+		for (auto &bb : F) {
+			// maps (base,index) to the last stored LLVM value
+			// { {base, index}, value }
+			std::map<std::pair<Value*, Value*>, Value*> avail;
+			std::vector<LoadInst*> dead;   // redundant load instructions to erase later
+
+			// GetElementPtr (GEP): array index calculation is done by GEP
+			// only do ptr offset, don't do store/load
+			// base: base ptr address
+			// index: offset from base
+
+			for (auto &inst : bb) {
+				// if Inst is a store to a single-index GetElementPtr
+				// StoreInst ex. ====> store i32 %val, i32* %ptr
+				// GEP ex. ====> %ptr = getelementptr <type>, <base>, <index 0>, <index 1>, ...
+				if (auto *SI = dyn_cast<StoreInst>(&inst)) {
+					if (auto *GEP = dyn_cast<GetElementPtrInst>(SI->getPointerOperand())) {
+
+						Value *base = GEP->getPointerOperand();
+						Value *index = GEP->getOperand(GEP->getNumOperands() - 1);   // index operand is the last operand
+						Value *val = SI->getValueOperand();
+
+						if (!isa<ConstantInt>(index)) {
+							for (auto it = avail.begin() ; it != avail.end() ; ) {
+								if ((it->first).first == base) {
+									it = avail.erase(it);    // return the following iterator
+								}
+								else {
+									it++;  // when not removing, move to next iterator
+								}
+							}
+						}
+						
+						avail[{base, index}] = val;
+					}
+				}
+				else if (auto *LI = dyn_cast<LoadInst>(&inst)) {
+					if (auto *GEP = dyn_cast<GetElementPtrInst>(LI->getPointerOperand())) {
+
+						Value *base = GEP->getPointerOperand();
+						Value *index = GEP->getOperand(GEP->getNumOperands() - 1);   // index operand is the last operand
+
+						if (avail.count({base, index})) {
+							Value *replacement = avail[{base, index}];
+							LI->replaceAllUsesWith(replacement);
+							dead.push_back(LI);
+
+							Changed = true;
+						}
+					}
+				}
+				else if (isa<CallInst>(inst)) {
+					avail.clear();    // the call may modify memory
+				}
+			}
+
+			for (auto *D : dead) {    // LoadInst*
+				Value *G = D->getPointerOperand();
+				D->eraseFromParent();
+				if (auto *GEP = dyn_cast<GetElementPtrInst>(G)) {
+					if (GEP->use_empty()) {
+						GEP->eraseFromParent();
+					}
+				}
+			}
+		}
+
 		return Changed;
 	}
 };
